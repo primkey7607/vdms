2ac7342c0bb2eae1ec0bad27a7dd040e9b07e445
mahircg <mahircan.guel@intel.com>
2019-07-12 14:42:20 +0200

    Add KeyFrameDecoder class
    This patch introduces 'KeyFrameDecoder' class, that can be used for
    partial video decoding. The class is initialized with a list of
    key-frame index/offset pairs, to be used for performing the partial
    video decoding.
    
    Signed-off-by: mahircg <mahircan.guel@intel.com>

--- include/vcl/KeyFrameParser.h
+++ include/vcl/KeyFrameParser.h
@@ -49,6 +49,9 @@ namespace VCL {
 
     using KeyFrameList = std::vector<KeyFrame>;
 
+    /*  *********************** */
+    /*    KEY_FRAME_PARSER      */
+    /*  *********************** */
     class KeyFrameParser {
 
     private:
@@ -79,5 +82,69 @@ namespace VCL {
 
         int init(void);
         int parse(KeyFrameList& frame_list);
+    };
+
+    /*  *********************** */
+    /*    KEY_FRAME_DECODER     */
+    /*  *********************** */
+    class KeyFrameDecoder {
+
+    private:
+
+        enum class H264Format {
+            AVCC   = 0,
+            AnnexB = 1
+        };
+
+        struct DecoderContext {
+            AVBSFContext    *bsf_context;
+            AVFormatContext *fmt_context;
+            AVCodecContext  *codec_context;
+            unsigned stream_index;
+            H264Format      byte_stream_format;
+
+            DecoderContext() : bsf_context(NULL), fmt_context(NULL),
+                               codec_context(NULL), stream_index(0),
+                               byte_stream_format(H264Format::AVCC) {};
+        };
+
+        struct FrameInterval {
+            KeyFrame start;
+            KeyFrame end;
+        };
+
+        struct DecodedKeyFrame {
+            AVFrame *frame;
+            uint64_t idx;
+        };
+
+        std::vector<std::pair<FrameInterval, std::vector<uint64_t>>> _interval_map;
+        std::vector<DecodedKeyFrame> _frame_list;
+        std::string    _filename;
+        DecoderContext _ctx;
+
+        int init_decoder(void);
+        int init_bsf(void);
+        int decode_interval(KeyFrame start, KeyFrame end,
+                            const std::vector<uint64_t> &frames);
+
+        int populate_interval_map(const KeyFrameList          &key_frames,
+                                  const std::vector<uint64_t> &frames);
+
+        int on_error(int errnum, const std::string opt = "");
+        void context_cleanup (void);
+
+    public:
+        KeyFrameDecoder() {};
+        KeyFrameDecoder(std::string filename): _filename(filename) {};
+        KeyFrameDecoder(KeyFrameDecoder&) = delete;
+        ~KeyFrameDecoder();
+
+        void set_filename(std::string filename);
+
+        int init(void);
+        int set_interval_map(const KeyFrameList          &key_frames,
+                             const std::vector<uint64_t> &frames);
+        int decode(void);
     };
 }
--- src/vcl/KeyFrameParser.cc
+++ src/vcl/KeyFrameParser.cc
@@ -29,9 +29,10 @@
  *
  */
 
+#include <algorithm>
+#include <errno.h>
 #include <iostream>
 #include <vector>
-#include <errno.h>
 
 #include "vcl/KeyFrameParser.h"
 
@@ -45,6 +46,9 @@ extern "C"
 
 using namespace VCL;
 
+/*  *********************** */
+/*    KEY_FRAME_PARSER      */
+/*  *********************** */
 int KeyFrameParser::init_stream(void) {
 
 	int ret = 0;
@@ -172,6 +176,274 @@ int KeyFrameParser::parse(KeyFrameList& frame_list) {
 }
 
 KeyFrameParser::~KeyFrameParser() {
+	context_cleanup();
+}
+
+/*  *********************** */
+/*    KEY_FRAME_DECODER     */
+/*  *********************** */
+int KeyFrameDecoder::init_decoder(void) {
+	int ret = 0;
+
+	_ctx.fmt_context = avformat_alloc_context();
+	ret = avformat_open_input(&_ctx.fmt_context,
+							  _filename.c_str(), NULL, NULL);
+	if (ret != 0)
+		return on_error(ret, _filename);
+
+	ret = avformat_find_stream_info(_ctx.fmt_context, NULL);
+	if (ret != 0)
+		return on_error(ret);
+
+	AVCodecParameters *codec = NULL;
+	for (unsigned i = 0; i < _ctx.fmt_context->nb_streams && !codec; i++) {
+		if (_ctx.fmt_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+			codec = _ctx.fmt_context->streams[i]->codecpar;
+			_ctx.stream_index = i;
+		}
+	}
+
+	if (!codec)
+		return on_error(AVERROR_ENCODER_NOT_FOUND, "No video codec");
+
+	if (codec->codec_id != AV_CODEC_ID_H264)
+		return on_error(AVERROR_EXTERNAL, "Input is not encoded with H264");
+
+	_ctx.byte_stream_format = (codec->bit_rate) ?
+							   H264Format::AVCC : H264Format::AnnexB;
+
+	const struct AVCodec *codec_ptr = avcodec_find_decoder(codec->codec_id);
+	if (!codec_ptr)
+		return on_error(AVERROR_DECODER_NOT_FOUND, "No video codec");
+
+	_ctx.codec_context = avcodec_alloc_context3(codec_ptr);
+	if (!_ctx.codec_context)
+		return on_error(AVERROR_DECODER_NOT_FOUND, "Failed to allocate alloc_context");
+
+	ret = avcodec_open2(_ctx.codec_context, codec_ptr, NULL);
+	if (ret < 0)
+		return on_error(ret);
+
+	return ret;
+}
+
+int KeyFrameDecoder::init_bsf(void){
+	int ret = 0;
+	const AVBitStreamFilter *bsf;
+
+	bsf = av_bsf_get_by_name("h264_mp4toannexb");
+	if (!bsf)
+		return on_error(AVERROR_BSF_NOT_FOUND, "av_bsf_get_by_name");
+
+	ret = av_bsf_alloc(bsf, &_ctx.bsf_context);
+	if (ret != 0)
+		return on_error(ret, "av_bsf_alloc");
+
+	AVRational time_base;
+	AVCodecParameters *codec;
+
+	time_base = _ctx.fmt_context->streams[_ctx.stream_index]->time_base;
+	codec     = _ctx.fmt_context->streams[_ctx.stream_index]->codecpar;
+
+	ret = avcodec_parameters_copy(_ctx.bsf_context->par_in, codec);
+	if (ret < 0)
+		return on_error(ret, "avcodec_parameters_copy");
+
+	_ctx.bsf_context->time_base_in = time_base;
+
+	ret = av_bsf_init(_ctx.bsf_context);
+	if (ret != 0)
+		return on_error(ret, "av_bsf_init");
+
+	return 0;
+}
+
+int KeyFrameDecoder::decode_interval(KeyFrame start, KeyFrame end,
+                                     const std::vector<uint64_t> &frames) {
+
+	AVPacket *pkt = av_packet_alloc();
+	if (!pkt)
+		return on_error(AVERROR_EXTERNAL, "av_packet_alloc");
+
+	AVFrame *current_frame = av_frame_alloc();
+	if (!current_frame)
+		return on_error(AVERROR_EXTERNAL, "av_frame_alloc");
+
+	int ret = 0;
+	if (_ctx.byte_stream_format == H264Format::AVCC) {
+		ret = av_seek_frame(_ctx.fmt_context, _ctx.stream_index, start.idx,
+                            AVSEEK_FLAG_FRAME);
+	}
+	else {
+		ret = av_seek_frame(_ctx.fmt_context, _ctx.stream_index, start.base,
+                            AVSEEK_FLAG_BYTE);
+	}
+
+	if (ret != 0)
+		return on_error(ret, "av_seek_frame");
+
+	avcodec_flush_buffers(_ctx.codec_context);
+
+	for (auto idx = start.idx; idx <= end.idx; ) {
+		do {
+			ret = av_read_frame(_ctx.fmt_context, pkt);
+			if (ret == AVERROR_EOF)
+				return on_error(AVERROR_EOF, "Encountered EOF while decoding");
+		} while (pkt->stream_index != _ctx.stream_index);
+
+		if (_ctx.byte_stream_format != H264Format::AnnexB)
+		{
+			ret = av_bsf_send_packet(_ctx.bsf_context, pkt);
+			if (ret != 0)
+				return on_error(ret, "av_bsf_send_packet");
+
+			ret = av_bsf_receive_packet(_ctx.bsf_context, pkt);
+			if (ret == AVERROR(EAGAIN))
+				continue;
+			else if (ret < 0)
+				return on_error(AVERROR(ret), "av_bsf_receive_packet");
+		}
+
+		ret = avcodec_send_packet(_ctx.codec_context, pkt);
+		if (ret < 0)
+			return on_error(AVERROR(ret), "Error sending a packet for decoding");
+
+		ret = avcodec_receive_frame(_ctx.codec_context, current_frame);
+		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+			continue;
+		else if (ret < 0)
+			return on_error(ret, "avcodec_receive_frame");
+		else if (ret == 0) {
+			auto found = std::find(frames.begin(), frames.end(), idx);
+			if (found != frames.end()) {
+				AVFrame *frame = av_frame_clone(current_frame);
+				_frame_list.push_back({.frame = frame, .idx = idx});
+			}
+		}
+		++idx;
+	}
+
+	av_frame_free(&current_frame);
+
+	av_packet_unref(pkt);
+
+	return 0;
+}
+
+int KeyFrameDecoder::populate_interval_map(
+	                               const std::vector<KeyFrame> &key_frames,
+	                               const std::vector<uint64_t> &frames)
+{
+	// TODO: check whether segment tree makes sense here
+
+	std::vector<KeyFrame> sorted_frame_list(key_frames);
+
+	if (sorted_frame_list.empty() || frames.empty())
+		return on_error(AVERROR_EXTERNAL,
+                        "Either key frame or query frame list is empty");
+
+	std::sort(sorted_frame_list.begin(), sorted_frame_list.end(),
+		      [&](KeyFrame l, KeyFrame r) { return l.idx < r.idx; });
+
+	for (auto i = 0; i < sorted_frame_list.size() - 1; ++i) {
+
+		FrameInterval interval = {.start = sorted_frame_list[i],
+		                          .end   = sorted_frame_list[i+1]};
+
+		std::vector<uint64_t> interval_frames;
+
+		for (auto &f : frames) {
+			if (f >= interval.start.idx && f < interval.end.idx)
+				interval_frames.push_back(f);
+		}
+
+		if (!interval_frames.empty())
+			_interval_map.push_back(std::make_pair(interval, interval_frames));
+	}
+
+	return 0;
+}
+
+int KeyFrameDecoder::on_error (int errnum, std::string opt) {
+	char errbuf[128];
+
+	int ret = av_strerror(errnum, errbuf, sizeof(errbuf));
+	if (ret != 0)
+		sprintf(errbuf, "unknown ffmpeg error");
+
+	std::string cause = "";
+	if (!opt.empty())
+		cause += (opt + ": ");
+
+	std::cerr << "*** " << cause << errbuf << std::endl;
+	std::cerr << "*** Key frame detection failed" << std::endl;
+
+	return errnum;
+}
+
+void KeyFrameDecoder::context_cleanup(void) {
+	for (auto &f : _frame_list) {
+		av_frame_free(&f.frame);
+	}
+	if (_ctx.fmt_context) {
+		avformat_close_input(&_ctx.fmt_context);
+		avformat_free_context(_ctx.fmt_context);
+	}
+	if (_ctx.codec_context) {
+		avcodec_close(_ctx.codec_context);
+	}
+	if (_ctx.bsf_context) {
+		av_bsf_free(&_ctx.bsf_context);
+	}
+}
+
+void KeyFrameDecoder::set_filename(std::string filename) {
+	_filename = filename;
+}
+
+int KeyFrameDecoder::init(void) {
+	if (_filename.empty()) {
+		std::cerr << "No filename" << std::endl;
+		return -1;
+	}
+
+	int ret = init_decoder();
+	if (ret != 0) {
+		context_cleanup();
+		return -1;
+	}
+
+	ret = init_bsf();
+	if (ret != 0) {
+		context_cleanup();
+		return -1;
+	}
+
+	return 0;
+}
+
+int KeyFrameDecoder::set_interval_map(
+                     const std::vector<KeyFrame> &key_frames,
+                     const std::vector<uint64_t> &frames) {
+
+	return populate_interval_map(key_frames, frames);
+
+}
+
+int KeyFrameDecoder::decode(void) {
+	int ret;
+
+	for (auto interval : _interval_map) {
+		ret = decode_interval(interval.first.start, interval.first.end,
+						      interval.second);
+		if (ret != 0)
+			return ret;
+	}
+
+	return 0;
+}
+
+KeyFrameDecoder::~KeyFrameDecoder() {
 	context_cleanup();
 }
 
